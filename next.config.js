f31ccb9a25bea216e584a1e35f7ac39b2c3cc7aa
r01wsen <75435686+r01wsen@users.noreply.github.com>
2026-06-14 17:30:44 -0500

    Update next.config.js - simplify for Next.js 15 compatibility

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-  images: {
-    domains: [],
-  },
 }
 
 module.exports = nextConfig

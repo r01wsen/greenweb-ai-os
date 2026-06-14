2b1dab2fda4b692523985b487a0b7ae780fa7499
r01wsen <75435686+r01wsen@users.noreply.github.com>
2026-06-14 17:23:49 -0500

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+  images: {
+    domains: [],
+  },
+}
+
+module.exports = nextConfig
